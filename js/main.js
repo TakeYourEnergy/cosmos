312ad8e32a7b4fdcc8c4fabdd3bee62b219d0460
Alexander <sashashvetsov3@gmail.com>
2021-10-27 20:20:32 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,28 @@
+$(function () {
+
+});
+
+// let howToText = document.querySelectorAll('.how-to__text')
+// let howToCircle = document.querySelectorAll('.how-to__circle')
+// console.log(howToText[0].getAttribute('fill'))
+
+
+// for (let i = 0; i < howToCircle.length; i++) {
+//    howToCircle[i].addEventListener('mouseenter', func)
+
+//    function func() {
+//       if (howToCircle[i] === howToCircle[0]) {
+//          howToText[0].setAttribute('fill', '#036CCA')
+//       }
+//       if (howToCircle[i] === howToCircle[1]) {
+//          howToText[1].setAttribute('fill', '#036CCA')
+//       }
+//    }
+
+// }
+
+
+
+
+
+
